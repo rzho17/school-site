e272069de1673f64f5e41aef77f310f8dd2dc788
Jogdeep <jogdeep8454@gmail.com>
2025-10-06 13:40:03 -0700

    changes commited

--- function.php
+++ function.php
@@ -21,9 +21,19 @@ function school_site_enqueues()
 }
 add_action('wp_enqueue_scripts', 'school_site_enqueues');
 // add styles to the back-end-editor
-function school_site_setup() {
-    add_editor_style( get_stylesheet_uri() );
-    add_theme_support('templates');
-    add_theme_support('parts');
+function school_site_setup(){
+    add_theme_support( 'post-thumbnails' );
+    //load styles at back end
+    add_editor_style(get_stylesheet_uri());
+     add_image_size('300x200', 300, 200, true);
+    add_image_size('800x400', 800, 400, true);
 }
 add_action( 'after_setup_theme', 'school_site_setup' );
+function school_add_custom_image_sizes( $size_names ) {
+	$new_sizes = array(
+		'300x200' => __( '300x200', 'school-site' ),
+		'800x400' => __( '800x400', 'school-site' ),
+	);
+	return array_merge( $size_names, $new_sizes );
+}
+add_filter( 'image_size_names_choose', 'school_add_custom_image_sizes' );
