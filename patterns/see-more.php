9825ee8e36b43b55a90251a463a6cd5fa9090e9d
Richard <richardzehungho@gmail.com>
2025-10-17 14:11:16 -0700

    add see more unsycned pattern

---
+++ patterns/see-more.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: See More Button
+ * Slug: school-site/see-more
+ * Categories: Buttons
+ */
+?>
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button"></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
